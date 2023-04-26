1312a6364187ea80f65888ed780513dc5b839222
stn <smrcouto88@gmail.com>
2023-04-26 16:12:00 -0300

    doodle and fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,7 @@ module.exports = {
       sans: ["Inter", "sans"],
       serif: ["Alegreya", "sans-serif"],
       mono: ["Unbounded", "sans-serif"],
+      title: ["Slayer", "cursive"],
     },
   },
   corePlugins: {
