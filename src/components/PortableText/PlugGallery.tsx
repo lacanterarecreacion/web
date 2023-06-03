f8d4d19af4950ae22eba3dcb8279578a21c0ba0d
stn <smrcouto88@gmail.com>
2023-06-03 14:18:00 -0300

    swiper react

---
+++ src/components/PortableText/PlugGallery.tsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { sanityConfig } from '@/lib/config';
+import imageUrlBuilder from "@sanity/image-url";
+
+const builder = imageUrlBuilder(sanityConfig);
+
+function urlFor(source: SanityImageSource) {
+  return builder.image(source);
+}
+
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+
+export default ({ gallery }: any) => {
+  return (
+    <>
+      <Swiper
+        key={gallery.images._key}
+        modules={[Navigation, Pagination, Scrollbar, A11y]}
+        spaceBetween={50}
+        slidesPerView={3}
+        navigation
+        pagination={{ clickable: true }}
+      >
+        {gallery.images.imagesGallery.map(
+          (item: { asset: any }, index: number) => {
+            return (
+              <SwiperSlide key={index}>
+                <a
+                  className="glightbox"
+                  href={urlFor(item.asset).width(1300).url()}
+                >
+                  <img
+                    className="object-cover w-full rounded-lg overflow-hidden h-64"
+                    src={urlFor(item).width(600).height(400).url()}
+                  />
+                </a>
+              </SwiperSlide>
+            );
+          }
+        )}
+      </Swiper>
+    </>
+  );
+};
---
+++ src/lib/config.tsx
@@ -0,0 +1,10 @@
+export const sanityConfig = {
+  // Find your project ID and dataset in `sanity.json` in your studio project
+  projectId: "1ekf84ne",
+  dataset: "production",
+  apiVersion: "2023-02-08",
+  useCdn: false,
+  // useCdn == true gives fast, cheap responses using a globally distributed cache.
+  // Set this to false if your application require the freshest possible
+  // data always (potentially slightly slower and a bit more expensive).
+};
