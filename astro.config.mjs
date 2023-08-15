251d29a313a3d81ed6042878b86bcc2a19201be7
stn <smrcouto88@gmail.com>
2023-08-15 16:48:04 -0300

    sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 import { defineConfig } from "astro/config";
-import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import image from "@astrojs/image";
 import sanity from "astro-sanity";
 import vue from "@astrojs/vue";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://lacanterarecreacion.org.ar/",
   integrations: [
-    mdx(),
     react(),
     tailwind(),
     image({
@@ -29,5 +29,6 @@ export default defineConfig({
         propsDestructure: true,
       },
     }),
+    sitemap(),
   ],
 });
